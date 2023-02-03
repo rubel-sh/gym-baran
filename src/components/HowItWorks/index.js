50aadf29ccfc31248c26df5e3f8131f87d5949c4
Rubel Hossain <rubrex96@hotmail.com>
2023-02-04 03:21:12 +0600

    Programs that can be taken section ui added

--- src/components/HowItWorks/index.js
+++ src/components/HowItWorks/index.js
@@ -1,5 +1,37 @@
+import Image from "next/image";
+import Container from "../Container";
+import rightArrow from "../../assets/icons/rightArrow.svg";
+import how_it_works from "../../assets/how_it_works.png";
+
 const HowItWorks = () => {
-  return <div>HowItWorks</div>;
+  return (
+    <Container>
+      <div className="grid md:grid-cols-2 text-primaryText">
+        {/* Left Content */}
+        <div className="self-start">
+          <Image src={how_it_works} />
+        </div>
+
+        {/* Right Content */}
+        <div className="grid place-items-center">
+          <div>
+            <h1 className="font-sporting text-5xl">
+              Best full body workout to lose fat
+            </h1>
+            <p className="font-poppins opacity-50 my-12">
+              Exercise is a very important need for our body. Health and fitness
+              will be obtained if you can do regular exercise and run a healthy
+              routine. Even at home we still have to be diligent in exercising,
+              either alone or with your friends at home.{" "}
+            </p>
+            <button className="py-4 px-6 flex justify-between items-center rounded-lg bg-secondaryColor text-white gap-7 cursor-pointer shadow-2xl">
+              <span>Get Started</span> <Image src={rightArrow} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HowItWorks;
--- src/components/OurPrograms/index.js
+++ src/components/OurPrograms/index.js
@@ -1,5 +1,103 @@
+import Image from "next/image";
+import Container from "../Container";
+import gymnastGirl from "../../assets/icons/gymnast-girl.png";
+import manYoga from "../../assets/icons/man-doing-yoga.png";
+import strengthen from "../../assets/icons/Group.png";
+import manYoga1 from "../../assets/icons/man-doing-yoga-.png";
+import practicingPosture from "../../assets/icons/person-practicing-a-strengthen-posture.png";
+import yogaPosture from "../../assets/icons/yoga-posture-.png";
+
 const OurPrograms = () => {
-  return <div>OurPrograms</div>;
+  return (
+    <Container>
+      <div className="text-primaryText">
+        <h1 className="sporting text text-5xl text-center leading-[73px]">
+          Programs that can <br /> be taken
+        </h1>
+        {/* Card Containers */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-14 gap-16">
+          <div className="flex ">
+            {/* icon */}
+            <div className="min-w-[80px] min-h-[80px] bg-gradient-to-r from-[#5478EF] to-[#9cb1ff]  grid place-items-center shadow-2xl rounded-full">
+              <Image src={gymnastGirl} className="" />
+            </div>
+            {/* texts */}
+            <div className="ml-5">
+              <p className="font-bold">Get ABS in 2 weekss</p>
+              <p className="opacity-50 py-1 pr-3">
+                Exercise is a very important need for our body.
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            {/* icon */}
+            <div className="min-w-[80px] min-h-[80px] bg-gradient-to-r from-[#4BACE1] to-[#96d4f6]  grid place-items-center shadow-2xl rounded-full">
+              <Image src={manYoga} className="" />
+            </div>
+            {/* texts */}
+            <div className="ml-5">
+              <p className="font-bold">25 Mins full body workout</p>
+              <p className="opacity-50 py-1 pr-3">
+                Exercise is a very important need for our body.
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            {/* icon */}
+            <div className="min-w-[80px] min-h-[80px] bg-gradient-to-r from-[#DD69C7] to-[#fbaced]  grid place-items-center shadow-2xl rounded-full">
+              <Image src={strengthen} className="" />
+            </div>
+            {/* texts */}
+            <div className="ml-5">
+              <p className="font-bold">10 Mins toned arms workout</p>
+              <p className="opacity-50 py-1 pr-3">
+                Exercise is a very important need for our body.
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            {/* icon */}
+            <div className="min-w-[80px] min-h-[80px] bg-gradient-to-r from-[#DD69C7] to-[#fbb1ee]  grid place-items-center shadow-2xl rounded-full">
+              <Image src={manYoga1} className="" />
+            </div>
+            {/* texts */}
+            <div className="ml-5">
+              <p className="font-bold">15 Mins full body fat burn</p>
+              <p className="opacity-50 py-1 pr-3">
+                Exercise is a very important need for our body.
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            {/* icon */}
+            <div className="min-w-[80px] min-h-[80px] bg-gradient-to-r from-[#5478EF] to-[#9eb3f9]  grid place-items-center shadow-2xl rounded-full">
+              <Image src={practicingPosture} className="" />
+            </div>
+            {/* texts */}
+            <div className="ml-5">
+              <p className="font-bold">25 Mins HIIT workout</p>
+              <p className="opacity-50 py-1 pr-3">
+                Exercise is a very important need for our body.
+              </p>
+            </div>
+          </div>
+          <div className="flex ">
+            {/* icon */}
+            <div className="min-w-[80px] min-h-[80px] bg-gradient-to-r from-[#4BACE1] to-[#96d4f6]   grid place-items-center shadow-2xl rounded-full">
+              <Image src={yogaPosture} className="" />
+            </div>
+            {/* texts */}
+            <div className="ml-5">
+              <p className="font-bold">Get ABS in 2 weeks</p>
+              <p className="opacity-50 py-1 pr-3">
+                Exercise is a very important need for our body.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default OurPrograms;
